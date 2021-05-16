e29a800a0deeedc816e7df5b2ce8374417ecf8fb
Denis <Denisssig+GitKraken@gmail.com>
2021-05-17 00:32:18 +0300

    JJ

---
+++ app/src/main/java/com/mirea/denisignatenko/practice_5/MainActivity.java
@@ -0,0 +1,44 @@
+package com.mirea.denisignatenko.practice_5;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mirea.denisignatenko.practice_5.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listCountSensor = findViewById(R.id.listView);
+        SensorManager sensorManager =
+                (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        ArrayList<HashMap<String, Object>> arrayList = new ArrayList<>();
+        HashMap<String, Object> sensorTypeList;
+        for (int i = 0; i < sensors.size(); i++) {
+            sensorTypeList = new HashMap<>();
+            sensorTypeList.put("Name", sensors.get(i).getName());
+            sensorTypeList.put("Value", sensors.get(i).getMaximumRange());
+            arrayList.add(sensorTypeList);
+        }
+
+        SimpleAdapter mHistory =
+                new SimpleAdapter(this, arrayList, android.R.layout.simple_list_item_2,
+                        new String[]{"Name", "Value"},
+                        new int[]{android.R.id.text1, android.R.id.text2});
+        listCountSensor.setAdapter(mHistory);
+    }
+}
